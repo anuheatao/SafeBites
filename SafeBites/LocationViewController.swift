e196704b9dc64886793e75aa951fb3b79e881424
athena <jiangqin.athena@gmail.com>
2020-10-01 20:22:42 -0700

    added api

--- SafeBites/LocationViewController.swift
+++ SafeBites/LocationViewController.swift
@@ -7,9 +7,16 @@
 //
 
 import UIKit
+import Foundation
+import CoreLocation
 
-class LocationViewController: UIViewController {
 
+class LocationViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
+    
+    var names: [String] = []
+    @IBOutlet var tableView: UITableView!
+    var location: CLLocation!
+    
     @IBAction func backtohomepressed(_ sender: Any) {
         self.performSegue(withIdentifier: "LocationtoHomeSegue", sender: self)
     }
@@ -17,10 +24,32 @@ class LocationViewController: UIViewController {
     @IBAction func toPerillaPressed(_ sender: Any) {
         self.performSegue(withIdentifier: "toPerillaSegue", sender: self)
     }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        if let x = UserDefaults.standard.object(forKey: "names") as? [String] {
+            names = x
+        }
+    
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "nameCell", for: indexPath) as! nameTableViewCell
+        cell.nameLabel.text = names[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //when you select it
+    }
+    
+    
 }
---
+++ SafeBites/nameTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  nameTableViewCell.swift
+//  SafeBites
+//
+//  Created by athena on 10/1/20.
+//  Copyright © 2020 Student. All rights reserved.
+//
+
+import UIKit
+
+class nameTableViewCell: UITableViewCell {
+
+    @IBOutlet var nameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
